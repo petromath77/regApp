e8b701681b572bffdf3b960e1948724264e35436
petromath77 <petromath77@gmail.com>
2018-08-30 17:42:42 +0300

    create

---
+++ src/app/app.module.ts
@@ -0,0 +1,39 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
+import { FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { GreetingComponent } from './greeting/greeting.component';
+import { HomeComponent } from './home/home.component';
+
+
+const routes: Routes = [
+  {path: '', redirectTo: '/', pathMatch: 'full'},
+  {path: '', component: HomeComponent},
+  {path: 'registration', component: RegistrationComponent},
+  {path: 'greeting', component: GreetingComponent},
+
+
+]
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    RegistrationComponent,
+    GreetingComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot(routes),
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
